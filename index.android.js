5ec24669ca64e703f0ef936600807ca27b912ed2
Rikharthu <uberviolence@gmail.com>
2016-12-20 22:33:39 +0200

    started navbar

--- index.android.js
+++ index.android.js
@@ -4,8 +4,8 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {AppRegistry, StyleSheet, Text, View, ScrollView, TouchableOpacity, Linking, ListView, Navigator } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View, ScrollView, TouchableOpacity, Linking, ListView, Navigator,TouchableHighlight } from 'react-native';
 import Header from './src/components/Header';
 import HourlyItem from './src/components/HourlyItem';
 import moment from 'moment';
@@ -25,115 +25,133 @@ export default class weather extends Component {
     fetch('https://api.darksky.net/forecast/53536b204bd8824a4c157697e0c24d7c/37.8267,-122.4233')
       .then(response => response.json())
       .then(responseJSON => {
-        this.setState({weather: responseJSON});
+        this.setState({ weather: responseJSON });
       });
   }
 
   render() {
     // console.log(this.state.weather);
 
+    const routes = [
+      {name: 'home', index: 0},
+      {name: 'hourly', index: 1}
+    ]
+
     return (
-        <Navigator
-          initialRoute={{name:'hourly'}}
-          renderScene={this.renderScene}
-        />        
+      <Navigator
+        initialRoute={{ name: 'home' }}
+        renderScene={this.renderScene}
+        navigationBar={
+          <Navigator.NavigationBar
+            routeMapper={{
+              LeftButton: (route, navigator, index, navState) => {
+                if (route.index === 0) {
+                  return null;
+                } else {
+                  return (
+                    <TouchableHighlight onPress={() => navigator.push(routes[1])}>
+                      <Text>Back</Text>
+                    </TouchableHighlight>
+                  );
+                }
+              },
+              RightButton: (route, navigator, index, navState) =>
+              { return (<Text>Done</Text>); },
+              Title: (route, navigator, index, navState) =>
+              { return (<Text>Awesome Nav Bar</Text>); },
+            }}
+            style={{ backgroundColor: 'gray' }}
+            />
+        }
+        />
     );
   }
 
   // depending on the required scene name return different components
   // TODO поясни разницу между renderscene(){ } и renderscene= () =>{}  (второй вариант принадлежит классу ( биндит к нему, и следовательно state его))
-  renderScene =(route, navigator)=>{
-    console.log('renderScene(), route='+route.name);
-    switch(route.name){
+  renderScene = (route, navigator) => {
+    console.log('renderScene(), route=' + route.name);
+    switch (route.name) {
       case 'home':
-        return(
-          <Text>HOME</Text>
+        return (
+          <CurrentForecast />
         );
-      case 'daily': 
-        return(
+      case 'daily':
+        return (
           <Text>daily</Text>
         );
-      case 'hourly': 
-        
-        return(
-          this.renderHourlyForecast()
+      case 'hourly':
+
+        return (
+          // this.renderHourlyForecast()
+          <HourlyForecast weather={this.state.weather} />
         );
     }
   }
-
-  renderHourlyForecast(){
-    console.log('rendering forecast')
-    if(this.state.weather != null){
-      const {currently, hourly, daily} = this.state.weather;
-      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-      dataSource = ds.cloneWithRows(hourly.data);
-      return (
-        <ListView
-          dataSource={dataSource}
-          renderRow={(rowData) => { 
-              // console.log(rowData);
-              // console.log(dataSource);
-              return <HourlyItem key={rowData.time} data={rowData}/> 
-            } 
-          }
-          renderFooter={()=><Button onPress={()=> Linking.openURL(this.darkSkyLink)}>More</Button>}
-          />
-        )
-    }else{
-      return <Text>Unable to load hourly forecast</Text>;
-    }    
-  }
-
-  renderDailyForecast(){
-    if(this.state.weather != null){
-      const {daily} = this.state.weather;
-      // TODO WTF?!?!?!?!?!
-      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-      // TODO WTF?!?!?!?!?!
-      dataSource = ds.cloneWithRows(hourly.data);
-      return (
-        // hourly.data.map(forecastItem => 
-        //   <HourlyItem key={forecastItem.time} data={forecastItem}/>
-        <ListView
-          dataSource={dataSource}
-          renderRow={(rowData) => <HourlyItem key={rowData.time} data={rowData}/>} />
-        )
-    }else{
-      return <Text>Unable to load daily forecast</Text>;
-    }
-  }
-
 }
 
-const HourlyForecast =(props) => {
-    console.log('rendering forecast')
-    if(props.weather != null){
-      const {currently, hourly, daily} = props.weather;
-      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-      dataSource = ds.cloneWithRows(hourly.data);
-      return (
-        <ListView
-          dataSource={dataSource}
-          renderRow={(rowData) => { 
-              // console.log(rowData);
-              // console.log(dataSource);
-              return <HourlyItem key={rowData.time} data={rowData}/> 
-            } 
-          }
-          renderFooter={()=><Button onPress={()=> Linking.openURL("")}>More</Button>}
-          />
-        )
-    }else{
-      return <Text>Unable to load hourly forecast</Text>;
-    }    
+const HourlyForecast = (props) => {
+  console.log('rendering forecast')
+  if (props.weather != null) {
+    const {currently, hourly, daily} = props.weather;
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    dataSource = ds.cloneWithRows(hourly.data);
+    return (
+      <ListView
+        dataSource={dataSource}
+        renderRow={(rowData) => {
+          // console.log(rowData);
+          // console.log(dataSource);
+          return <HourlyItem key={rowData.time} data={rowData} />
+        }
+        }
+        renderFooter={() => <Button onPress={() => Linking.openURL("")}>More</Button>}
+        />
+    )
+  } else {
+    return <Text>Unable to load hourly forecast</Text>;
   }
+}
+
+const DailyForecast = (props) => {
+  console.log('rendering forecast')
+  if (props.weather != null) {
+    const {currently, hourly, daily} = props.weather;
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    dataSource = ds.cloneWithRows(hourly.data);
+    return (
+      <ListView
+        dataSource={dataSource}
+        renderRow={(rowData) => {
+          // console.log(rowData);
+          // console.log(dataSource);
+          return <HourlyItem key={rowData.time} data={rowData} />
+        }
+        }
+        renderFooter={() => <Button onPress={() => Linking.openURL("")}>More</Button>}
+        />
+    )
+  } else {
+    return <Text>Unable to load hourly forecast</Text>;
+  }
+}
+
+const CurrentForecast = (props) => {
+  return (
+    <View>
+      <Text>Hello</Text>
+      <Text>World</Text>
+      <Text>!</Text>
+    </View>
+  );
+}
 
 const styles = {
-    buttonContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-    }
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  }
 };
 
 AppRegistry.registerComponent('weather', () => weather);
